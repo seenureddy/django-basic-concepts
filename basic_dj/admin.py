4860431c3aa7bb76e07e0baf78120d627713233c
seenureddy <srinivasulureddy.sura30@gmail.com>
2014-10-06 15:15:32 +0530

    adding prepopulated field in admin so it will generate auto slug in admin

--- basic_dj/admin.py
+++ basic_dj/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from basic_dj.models import Publisher, Author, Book
 
+
+class AuthorAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['email']}
+
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ['title']}
+
+
 admin.site.register(Publisher)
-admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
