4da143c268d64b9bbd4d03469334adbf1e6faad3
seenureddy <srinivasulureddy.sura30@gmail.com>
2014-10-08 19:19:47 +0530

    changed indentation

--- basic_dj/tests/test_views.py
+++ basic_dj/tests/test_views.py
@@ -11,38 +11,29 @@ class PublisherViewTest(TestCase):
 
     def _login(self):
         User.objects.create_user(username='foo', password='bar')
-        # response = self.client.get(reverse('create_publisher'))
-        # self.assertEqual(response.status_code, 200)
         self.client.login(username='foo', password='bar')
-        # self.assertEqual(response.status_code, 302)
 
     def test_create_publisher(self):
-        # User.objects.create_user(username='foo', password='bar')
-        # response = self.client.get(reverse('create_publisher'))
-        # self.assertEqual(response.status_code, 302)
-        # self.client.login(username='foo', password='bar')
-        # response = self.client.get(reverse("create_publisher"))
-        # self.assertEqual(response.status_code, 200)
         self._login()
-        response = self.client.post(reverse('create_publisher'), {
-                                    'name': 'test',
-                                    'address': '4/333 Telengana Hyderabad',
-                                    'city': 'Hyderabad',
-                                    'state_province': 'Telengana',
-                                    'country': 'INDIA',
-                                    'website': 'https://www.google.com'})
+        response = self.client.post(reverse('create_publisher'),
+                                    {'name': 'test',
+                                     'address': '4/333 Telengana Hyderabad',
+                                     'city': 'Hyderabad',
+                                     'state_province': 'Telengana',
+                                     'country': 'INDIA',
+                                     'website': 'https://www.google.com'})
         self.assertEqual(response.status_code, 200)
 
-        def test_create_publisher_with_publisher_file_upload(self):
-            self._login()
-            testfile = tempfile.TemporaryFile()
-            testfile.write('Some data happy life')
-            response = self.client.post(reverse('create_publisher'), {
-                                        'name': 'test',
-                                        'address': '4/333 Telengana Hyderabad',
-                                        'city': 'Telengana',
-                                        'state_province': 'Hyderabad',
-                                        'country': 'INDIA',
-                                        'website': 'https://www.google.com',
-                                        'publisher_file': testfile})
-            self.assertEqual(response.status_code, 302)
+    def test_create_publisher_with_publisher_file_upload(self):
+        self._login()
+        testfile = tempfile.TemporaryFile()
+        testfile.write('Some data happy life')
+        response = self.client.post(reverse('create_publisher'),
+                                    {'name': 'test',
+                                     'address': '4/333 Telengana Hyderabad',
+                                     'city': 'Telengana',
+                                     'state_province': 'Hyderabad',
+                                     'country': 'INDIA',
+                                     'website': 'https://www.google.com',
+                                     'publisher_file': testfile})
+        self.assertEqual(response.status_code, 302)
