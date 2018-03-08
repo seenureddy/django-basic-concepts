6d3ee4fc27a587467959086baf14ac82aa4e99c7
seenureddy <srinivasulureddy.sura30@gmail.com>
2018-03-08 13:14:05 +0530

    Updated code to support django latest version

--- basic_dj/urls.py
+++ basic_dj/urls.py
@@ -1,21 +1,21 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-urlpatterns = patterns('basic_dj.views',
-                       url(r'^$', 'index', name='index'),
-                       url(r'^create-publisher/$', 'create_publisher',
-                           name='create_publisher'),
-                       url(r'^detail-publisher/(?P<publisher_slug>[-\w]+)/$',
-                           'detail_publisher', name='detail_publisher'),
-                       url(r'^create-book/$',
-                           'create_book', name='create_book'),
-                       url(r'^detail-book/(?P<book_slug>[-\w]+)/$',
-                           'detail_book', name='detail_book'),
-                       url(r'^create-author/$',
-                           'create_author', name='create_author'),
-                       url(r'^books/$', 'book_list', name='book_list'),
-                       url(r'^books/author/(?P<author_slug>[-\w]+)/$',
-                           'author_books_list',
-                           name='author_books_list'),
-                       url(r'^books/publisher/(?P<publisher_slug>[-\w]+)/$',
-                           'publisher_books_list',
-                           name='publisher_books_list'), )
+
+urlpatterns = [
+    url(r'^$', 'index', name='index'),
+    url(r'^create-publisher/$', 'create_publisher',
+        name='create_publisher'),
+    url(r'^detail-publisher/(?P<publisher_slug>[-\w]+)/$',
+        'detail_publisher', name='detail_publisher'),
+    url(r'^create-book/$',
+        'create_book', name='create_book'),
+    url(r'^detail-book/(?P<book_slug>[-\w]+)/$',
+        'detail_book', name='detail_book'),
+    url(r'^create-author/$',
+        'create_author', name='create_author'),
+    url(r'^books/$', 'book_list', name='book_list'),
+    url(r'^books/author/(?P<author_slug>[-\w]+)/$',
+        'author_books_list', name='author_books_list'),
+    url(r'^books/publisher/(?P<publisher_slug>[-\w]+)/$',
+        'publisher_books_list', name='publisher_books_list'),
+]
--- django_concept/urls.py
+++ django_concept/urls.py
@@ -1,17 +1,17 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-admin.autodiscover()
 
 handler404 = 'basic_dj.views.error404'
-urlpatterns = patterns('',
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^library/', include('basic_dj.urls')),
-                       url(r'^$', 'django_concept.views.home', name='home'),
-                       url(r'^braintree/clienttoken/', 'braintreeapp.views.clienttoken', name='client_token'),
-                       url(r'^braintree/checkout/', 'braintreeapp.views.checkout', name='payments_checkout'),
-                       )
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^library/', include('basic_dj.urls')),
+    url(r'^$', 'django_concept.views.home', name='home'),
+    url(r'^braintree/clienttoken/', 'braintreeapp.views.clienttoken', name='client_token'),
+    url(r'^braintree/checkout/', 'braintreeapp.views.checkout', name='payments_checkout'),
+]
 
 urlpatterns += static(settings.MEDIA_URL,
                       document_root=settings.MEDIA_ROOT)
